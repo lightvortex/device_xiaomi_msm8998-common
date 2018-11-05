375d4c977a41ed29da5f5bbac36c65ed35b79a00
Jeff Tinker <jtinker@google.com>
2018-11-05 15:05:26 -0800

    msm8998-common: Enable clearkey drm plugin v1.2
    bug:117570956
    bug:110838441
    bug:116252891
    
    test: android.media.cts.MediaDrmClearkeyTest#testOfflineKeyManagement
    
    Change-Id: I03f3e298148dc388f73ce83681434a5802fde68b

--- msm8998.mk
+++ msm8998.mk
@@ -177,7 +177,7 @@ $(call inherit-product, build/target/product/verity.mk)
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.2-service.clearkey
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
