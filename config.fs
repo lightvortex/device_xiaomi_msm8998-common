e525d0c7d38b8c24a4915cf0d7ef03e3d76b5044
Michael Bestas <mkbestas@lineageos.org>
2019-10-01 21:14:38 +0300

    msm8998-common: Remove most rootdir symlinks and /dsp sepolicy
    * No longer needed with pie blobs
    * Keep /persist for now because xiaomi additions to blobs
      still require it :/
    
    Change-Id: I3495fe5a3475c24c41eaedcb830fb12dfe395e44

--- config.fs
+++ config.fs
@@ -1,6 +1,27 @@
 [AID_VENDOR_QTI_DIAG]
 value:2901
 
+[AID_VENDOR_QDSS]
+value:2902
+
+[AID_VENDOR_RFS]
+value:2903
+
+[AID_VENDOR_RFS_SHARED]
+value:2904
+
+[AID_VENDOR_ADPL_ODL]
+value:2905
+
+[AID_VENDOR_QRTR]
+value:2906
+
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
+
 [vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
@@ -43,25 +64,13 @@ user:  AID_GPS
 group: AID_GPS
 caps: SETUID SETGID
 
-[firmware/]
-mode: 0771
+[vendor/bin/sensors.qti]
+mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: 0
-
-[firmware/image/*]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
+caps: NET_BIND_SERVICE
 
 [vendor/firmware_mnt/image/*]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[bt_firmware/]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -71,10 +80,4 @@ caps: 0
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: 0
-
-[dsp/]
-mode: 0771
-user: AID_MEDIA
-group: AID_MEDIA
 caps: 0
