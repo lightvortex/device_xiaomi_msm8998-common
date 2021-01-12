230921e074d2e5aa6e5432f4f9c195a6b94e99a0
WhiterWalt <zalunai32@gmail.com>
2021-01-12 17:35:40 +0000

    rework old branch

--- system_prop.mk
+++ system_prop.mk
@@ -17,3 +17,8 @@
 # Camera
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vendor.camera.aux.packagelist=com.android.camera,org.lineageos.snap
+
+# Blur
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
